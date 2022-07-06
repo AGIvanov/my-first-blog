2154669c07f352bfe9bc840730ef4b67a2d392ed
Artemio <admin@admin.com>
2022-07-06 15:47:57 +0300

    Can access to admin panel: deleted SESSION_COOKIE_DOMAIN = ['127.0.0.1', '.eu.ngrok.io']

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,5 +121,3 @@ LOGIN_REDIRECT_URL = '/'
 BASE_URL = "http://127.0.0.1:8000"
 DEV_SERVER = len(sys.argv) > 1 and sys.argv[1] == "runserver"
 USE_NGROK = os.environ.get("USE_NGROK", "False") == "True" and os.environ.get("RUN_MAIN", None) != "true"
-
-SESSION_COOKIE_DOMAIN = ['127.0.0.1', '.eu.ngrok.io']
